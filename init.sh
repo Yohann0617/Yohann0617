9db93d14e66bb46395ee43cc9eaa9540c5571b4c
Yohann <75626191+Yohann0617@users.noreply.github.com>
2024-07-31 19:30:41 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -25,9 +25,10 @@ grep -qxF 'source ~/.bashrc' "$PROFILE_FILE" || echo 'source ~/.bashrc' >> "$PRO
 
 # 使更改生效
 source "$PROFILE_FILE"
-
-# 重新启动 shell 使更改生效
-exec bash
+# 使更改生效
+source ~/.bashrc
+# 使更改生效
+source "$PROFILE_FILE"
 
 # 定义颜色
 BLACK='\033[0;30m'
